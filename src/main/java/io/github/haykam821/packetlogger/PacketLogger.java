f3037a0e00fc05eb0cbe057ae4726d60b1b8be6b
Kale-Ko <github@kaleko.dev>
2024-09-18 20:51:58 -0400

    Log packet fields

--- src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
+++ src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
@@ -1,5 +1,7 @@
 package io.github.haykam821.packetlogger;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Locale;
 import net.minecraft.network.NetworkSide;
 import net.minecraft.network.packet.Packet;
@@ -21,15 +23,45 @@ public class PacketLogger {
         return side.name().toLowerCase(Locale.ROOT);
     }
 
-    public static void logSentPacket(Packet<?> packet, NetworkSide side) {
-        String sideName = PacketLogger.getSideName(side);
+    public static String makePacketString(Packet<?> packet) {
+        StringBuilder packetString = new StringBuilder(packet.getClass().getName());
+        packetString.append("[");
 
-        LOGGER.info("Sending packet with name '{}' ({})", packet.getClass().getName(), sideName);
+        Field[] fields = packet.getClass().getDeclaredFields();
+        boolean addComma = false;
+        for (Field field : fields) {
+            if ((field.getModifiers() & Modifier.STATIC) == Modifier.STATIC || "CODEC".equals(field.getName())) {
+                continue;
+            }
+
+            if (addComma) {
+                packetString.append(",");
+            }
+            addComma = true;
+
+            packetString.append(field.getName());
+            packetString.append("=");
+            if (field.trySetAccessible()) {
+                try {
+                    Object value = field.get(packet);
+                    packetString.append(value);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                packetString.append("<error>");
+            }
+        }
+
+        packetString.append("]");
+        return packetString.toString();
+    }
+
+    public static void logSentPacket(Packet<?> packet, NetworkSide side) {
+        LOGGER.info("Sending packet {} ({})", PacketLogger.makePacketString(packet), PacketLogger.getSideName(side));
     }
 
     public static void logReceivedPacket(Packet<?> packet, NetworkSide side) {
-        String sideName = PacketLogger.getSideName(side);
-
-        LOGGER.info("Received packet with name '{}' ({})", packet.getClass().getName(), sideName);
+        LOGGER.info("Receiving packet {} ({})", PacketLogger.makePacketString(packet), PacketLogger.getSideName(side));
     }
 }
